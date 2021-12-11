61a8cd77a5e82b7b8aa34de68d042eb950f4235c
Lei Gao <leigao@Leis-MacBook-Pro.local>
2021-12-10 23:57:17 -0800

    Update build_android.sh

--- build_android.sh
+++ build_android.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ANDROID_DIR=/data/local/tmp
+
 cmake ../ \
 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
 -DCMAKE_BUILD_TYPE=Release \
@@ -6,3 +8,10 @@ cmake ../ \
 -DANDROID_NATIVE_API_LEVEL=android-21
 
 make
+adb push demo /data/local/tmp
+
+EPOCH=1
+LOSS_SCALAR=1
+LR=0.01
+PRECISION="fp32"
+adb shell "LD_LIBRARY_PATH=$ANDROID_DIR $ANDROID_DIR/demo $ANDROID_DIR/mnist_data $EPOCH $LOSS_SCALAR $LR $PRECISION"
